44b95e1e2d4bb299bf007b954a89317f94359741
chris s <test@test.com>
2025-11-24 13:47:47 +0100

    added example controller feature

---
+++ backend/src/main/java/de/csausl/backend/controller/exampleController.java
@@ -0,0 +1,10 @@
+package de.csausl.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/example")
+public class exampleController {
+
+}
