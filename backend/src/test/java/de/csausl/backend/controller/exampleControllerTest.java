8526f4821d171429a85c6f48c21d8bf811bf89eb
chris s <test@test.com>
2025-11-25 10:43:58 +0100

    added test

---
+++ backend/src/test/java/de/csausl/backend/controller/exampleControllerTest.java
@@ -0,0 +1,12 @@
+package de.csausl.backend.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class exampleControllerTest {
+
+    @Test
+    void helloExample() {
+        assert(true);
+    }
+}
